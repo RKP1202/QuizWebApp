4ca055630bb3ec3341d345c4a4939eb2cbb7838b
RKP1202 <b22es003@iitj.ac.in>
2024-12-17 13:43:59 +0530

    Added functionalities

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Question, Student
+from django.contrib import messages
+import random
 
-# Create your views here.
+# Starting the quiz
+def start_quiz(request):
+    random_question = Question.objects.order_by('?').first()
+
+    options = random_question.options.all()
+
+    context = {
+        'question': random_question,
+        'options': options,
+    }
+    return render(request, 'quiz/question.html', context)
+
+
+def has_taken_quiz(request):
+    if request.method == "POST":
+        username = request.POST.get("username").upper()
+        student, created = Student.objects.get_or_create(username=username)
+
+        if student.status == "done":
+            messages.error(request, f'{username} has already taken the quiz.')
+            return redirect('start_quiz')
+
+        messages.success(request, "Proceed to take your quiz.")
+        return redirect('start_quiz')
+
+#  quiz submission
+def submit_quiz(request):
+    if request.method == "POST":
+        username = request.POST.get("username").upper()
+        score = int(request.POST.get("score"))
+        
+        student = Student.objects.get(username=username)
+        student.score = score
+        student.status = "done"
+        student.save()
+
+        return redirect('quiz_submitted')  
+
+def quiz_submitted(request):
+    return render(request, 'quiz/submission_success.html')
